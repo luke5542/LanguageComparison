9cd8cf929330751e758c24014f7ddd859d47c267
Dan Underwood <daniel.underwood@student.manchester.ac.uk>
2015-06-06 18:24:45 +0100

    Adding Python

---
+++ sieve-py.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+max = int(sys.argv[1])
+
+primes = [False, False]
+primes.extend([True] * (max - 2))
+
+for num in range(2, math.ceil(math.sqrt(max))):
+    if primes[num]:
+        for square in range(int(math.pow(num, 2)), max, num):
+            primes[square] = False
+
+print("Number of primes: " + str(primes.count(True)))
