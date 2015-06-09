10502d0779580e6c1aea0e59502ed10d7e5dff60
Dan Underwood <daniel.underwood@student.manchester.ac.uk>
2015-06-09 23:05:30 +0100

    Shell version and optimisations

--- sieve-js.js
+++ sieve-js.js
@@ -5,14 +5,16 @@ for (var num = 2; num <= max; num++) {
   primes[num] = true;
 }
 
+count = max - 2;
 for (var num = 2; num <= Math.sqrt(max); num++) {
   if (primes[num]) {
     for (var square = Math.pow(num, 2); square < max + 1; square += num) {
+      if (primes[square]) {
+        count--;
+      }
       primes[square] = false;
     }
   }
 }
 
-console.log('Number of primes: ' + primes.filter(function(value) {
-    return value === true;
-}).length);
+console.log('Number of primes: ' + count);
--- sieve-py.py
+++ sieve-py.py
@@ -5,9 +5,12 @@ max = int(sys.argv[1])
 
 primes = [False] * 2 + [True] * (max - 1)
 
+count = max - 2
 for num in range(2, int(math.ceil(math.sqrt(max)))):
     if primes[num]:
         for square in range(int(num**2), max + 1, num):
+            if primes[square]:
+                count -= 1
             primes[square] = False
 
-print("Number of primes: " + str(primes.count(True)))
+print("Number of primes: " + str(count))
