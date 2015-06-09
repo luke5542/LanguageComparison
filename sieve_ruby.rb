54bf809ac601cf55371694e7bbedf660346604f0
Dan Underwood <daniel.underwood@student.manchester.ac.uk>
2015-06-09 23:14:31 +0100

    Switching back to using built-in counts for scripts, since faster

--- sieve_ruby.rb
+++ sieve_ruby.rb
@@ -2,14 +2,10 @@ max = ARGV[0].to_i
 
 primes = Array.new(2, false) + Array.new(max - 1, true)
 
-count = max - 2
 (2..Math.sqrt(max)).each do |num|
   (num**2..max + 1).step(num) do |square|
-    if primes[square]
-      count -= 1
-    end
     primes[square] = false
   end if primes[num]
 end
 
-puts "Number of primes: #{count}"
+puts "Number of primes: #{primes.count(true)}"
