fd7f84cea1cfa91cb60705ef9f1e7b7120275bd6
Dan Underwood <dan.underwood@outlook.com>
2015-06-30 15:30:15 +0100

    Improved Ruby timing

--- sieve_ruby.rb
+++ sieve_ruby.rb
@@ -1,16 +1,17 @@
+require 'benchmark'
+
 max = ARGV[0].to_i
+primes = nil
 
-time_s = Time.now
+time = Benchmark.realtime do
+  primes = Array.new(2, false) + Array.new(max - 1, true)
 
-primes = Array.new(2, false) + Array.new(max - 1, true)
-
-(2..Math.sqrt(max)).each do |num|
-  (num**2..max + 1).step(num) do |square|
-    primes[square] = false
-  end if primes[num]
+  (2..Math.sqrt(max)).each do |num|
+    (num**2..max + 1).step(num) do |square|
+      primes[square] = false
+    end if primes[num]
+  end
 end
 
-time_e = Time.now
-
 puts "Number of primes: #{primes.count(true)}"
-puts "Execution time: #{((time_e - time_s) * 1000).round(3)}ms"
+puts "Execution time: #{((time) * 1000).round(3)}ms"
